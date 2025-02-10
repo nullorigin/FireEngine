d9d59a69de7428ca93b8fa32290c603c0ad245a5
Matthew Fay <mattfay@pm.me>
2025-02-09 21:11:06 -0500

    First upload to crates.io prep.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #[derive(Debug, Clone)]
-struct info {
+struct ProgramInfo {
     version: String,
     name: String,
     about: String,
     intro: String,
 }
 
-fn get_info() -> info {
-    info {
+fn get_program_info() -> ProgramInfo {
+    ProgramInfo {
         version: String::from("0.1.0"),
         name: String::from("FireEngine"),
         about: String::from("FireEngine is written in Rust. \nThis program generates a firewall configuration based on a set of specified logfiles."),
@@ -15,7 +15,7 @@ fn get_info() -> info {
     }
 }
 fn main() {
-    println!("{}", get_info().intro);
-    println!("{}", get_info().about);
+    println!("{}", get_program_info().intro);
+    println!("{}", get_program_info().about);
     println!();
 }
